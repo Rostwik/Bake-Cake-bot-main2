dafec65ea95fbc2fc92486a3171df44c34a61ff6
Rostislav <rostwik@gmail.com>
2021-11-01 14:05:31 +0300

    Static

--- Bake_Cake_bot/Bake_Cake_bot/settings.py
+++ Bake_Cake_bot/Bake_Cake_bot/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 import dj_database_url
@@ -117,9 +117,13 @@ USE_L10N = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Bake_Cake_bot/Bake_Cake_bot/wsgi.py
+++ Bake_Cake_bot/Bake_Cake_bot/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Bake_Cake_bot.settings')
 
 application = get_wsgi_application()
